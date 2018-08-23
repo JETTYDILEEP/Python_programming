39c17f1f045b7301adccd1eaceac19dd07dd292b
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2018-08-23 16:44:33 +0530

    Update typeword.py

--- typeword.py
+++ typeword.py
@@ -1,4 +1,4 @@
-w = str(raw_input("enter word"))
+w = str(raw_input(""))
 if(w=='o' or w=='i' or w=='e' or w=='a' or w=='u'):
   print("Vowel")
 elif(w=='b' or w=='c' or w=='d' or w=='f' or w=='g' or w=='h' or w=='j' or w=='k' or w=='l' or w=='m' or w=='n' or w=='p' or w=='q' or w=='r' or w=='s' or w=='t' or w=='v' or w=='w' or w=='x' or w=='y' or w=='z'):
