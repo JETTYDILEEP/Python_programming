8979f77614f2438c4acc6d80dd1d4348a79fee27
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2018-08-23 16:40:49 +0530

    Create typeword.py

---
+++ typeword.py
@@ -0,0 +1,7 @@
+w = str(raw_input("enter word"))
+if(w=='o' or w=='i' or w=='e' or w=='a' or w=='u'):
+  print("Vowel")
+elif(w=='b' or w=='c' or w=='d' or w=='f' or w=='g' or w=='h' or w=='j' or w=='k' or w=='l' or w=='m' or w=='n' or w=='p' or w=='q' or w=='r' or w=='s' or w=='t' or w=='v' or w=='w' or w=='x' or w=='y' or w=='z'):
+  print("Consonant")
+else:
+   print("invalid") 
