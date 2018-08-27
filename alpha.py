8c128e8192a712916cc314721d4c620a677b8dcb
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2018-08-27 16:51:06 +0530

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -1,5 +1,5 @@
-ch = str(raw_input("Enter a character: "))
+ch = str(raw_input(""))
 if((ch>='a' and ch<= 'z') or (ch>='A' and ch<='Z')):
-  print (ch, "is an Alphabet")
+  print ("Alphabet")
 else:
-  print(ch, "is not an Alphabet")
+  print("No")
