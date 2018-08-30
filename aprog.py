7944f01f35c17042167be2e78fc03ecb0ca2ee29
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2018-08-30 15:01:22 +0530

    Update aprog.py

--- aprog.py
+++ aprog.py
@@ -2,10 +2,10 @@ n = int(input(" "))
 a = int(input(" "))
 d = int(input(" "))
 k=0
-if(n>=1 and n<=100000 , a>=1 and a<=100000 , d>=1 and d<=1000):
+if(n>0 and n<=100000 , a>0 and a<=100000 , d>0 and d<=1000):
   for x in range (1,n):
     t = a+(n-1)*d
-    k =  a+(n-1)*d
+    k =t
     l=t+k
     
 print(l)
