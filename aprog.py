215fa6c01e537ec16d369e5f9f3805f93eb6802f
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2018-08-30 14:41:14 +0530

    Create aprog.py

---
+++ aprog.py
@@ -0,0 +1,11 @@
+n = int(input(" "))
+a = int(input(" "))
+d = int(input(" "))
+k=0
+if(n>=1 and n<=100000 , a>=1 and a<=100000 , d>=1 and d<=1000):
+  for x in range (1,n):
+    t = a+(n-1)*d
+    k =  a+(n-1)*d
+    l=t+k
+    
+print(l)
