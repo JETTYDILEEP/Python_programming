2dc56349d2be168bf2165507e2c3c77638e4849c
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2018-09-06 12:32:28 +0530

    Update fibona.py

--- fibona.py
+++ fibona.py
@@ -1,10 +1,10 @@
 def fib(n):
-	if n==1 or n==0:
+	if n==1 or n==1:
 		return(n)
 	else :
 		return fib(n-1)+fib(n-2)
 try:
-	n=int(input('Enter n :'))
+	n=int(input(''))
 	sum=0
 	for i in range(0,n):
 		print(fib(i))
