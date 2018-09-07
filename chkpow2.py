d43bbdf2f393433ac525eee190d1975474fb64db
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2018-09-07 12:11:13 +0530

    Update chkpow2.py

--- chkpow2.py
+++ chkpow2.py
@@ -1,16 +1,12 @@
-def main():
-	try:
-		k=0
-		n=int(input())
-		while(n!=0):
-			n=n/2
-			if n==1:
-				k=1
-				break
-		if k!=1:
-			print('no')
-		else :
-			print('yes')
-	except:
-		print('invalid')
-main()
+f=0
+n=int(input())
+while(n!=0):
+  n=n/2
+  if n==1:
+    f=1
+    break
+if f!=1:
+	print('no')
+else :
+	print('yes')
+
