87ceef57bc960f5c48a79da8d42712f4f4fddd41
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2018-09-08 10:21:00 +0530

    Update alphanum.py

--- alphanum.py
+++ alphanum.py
@@ -1,13 +1,16 @@
 n = input("")
 j=0
 k=0
+m=0
 for i in n:
   if(i.isalpha()):
     j=j+1
   elif(i.isnumeric()):
     k=k+1
+  elif(i=='_'):
+    m=m+1  
 
-if(j+k==len(n) and j>0 and k>0):
+if(j+k+m==len(n) and j>0 and k>0 and m>0):
   print("yes")
 else:
   print("no")      
