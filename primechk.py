627c12fbeca0a700dcc79fc4948f5be17492d024
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2018-09-09 06:55:44 +0530

    Update primechk.py

--- primechk.py
+++ primechk.py
@@ -1,8 +1,8 @@
-num = int(input(""))
-if num > 1:
+n = int(input(""))
+if n > 1:
    
-   for i in range(2,num):
-       if (num % i) == 0:
+   for i in range(2,n):
+       if (n % i) == 0:
            print("no")
            break
    else:
