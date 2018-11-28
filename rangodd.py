fdd00d916b3a0a2c12ac08fc023e2feda8885330
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2018-11-28 12:35:40 +0530

    Update rangodd.py

--- rangodd.py
+++ rangodd.py
@@ -1,6 +1,7 @@
 n,m = map(int,input().split())
-for x in range(n+1,m):
-  if(x%2!=0):
-    print(x,end=' ')
-  else:
-    print(x,end="")  
+for i in range(n+1,m):
+  if i%2==0:
+    if(i<m-2):
+      print(i,end=' ')
+    else:
+      print(i,end="")  
