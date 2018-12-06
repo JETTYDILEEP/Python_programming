bd30c7511f43cbfcbac29af80cdd31537e834d95
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2018-12-06 10:42:17 +0530

    Update rangprim.py

--- rangprim.py
+++ rangprim.py
@@ -1,6 +1,5 @@
-lower = int(input(" "))
-upper = int(input(" "))
-for num in range(lower,upper + 1):
+lower,upper = map(int,input().split())
+ for num in range(lower,upper + 1):
    if num > 1:
        for i in range(2,num):
            if (num % i) == 0:
